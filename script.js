70c4e3d6ab1e28d4804171d45e19ef9aa7316986
Austin Adams <austin@farmingtonwebdesign.net>
2015-07-07 10:45:04 -0700

    Added a controller

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Code goes here
 angular.module('myApp', [])
+  .filter('niceName', function(){
+    return function(input){
+      var spaced = input.replace('_',' '),
+        capped = spaced.replace( /\b\w/g, function (m) {
+          return m.toUpperCase();
+        });
+      return capped;
+    };
+  })
   .controller("storyController", function ($scope) {
     $scope.name,
       $scope.job_title,
@@ -8,7 +17,7 @@ angular.module('myApp', [])
       $scope.dirty_task,
       $scope.useless_skill,
       $scope.adjective,
-      $scope.obnoxiuous_celbertity,
+      $scope.obnoxiuous_celebrity,
       $scope.huge_number = "";
     $scope.gender = 'F';
     $scope.genderStrFn = function(){
@@ -17,4 +26,10 @@ angular.module('myApp', [])
       $scope.genderStr3 = $scope.gender === 'F' ? "her":"him";
     };
     $scope.genderStrFn();
+
+    $scope.buildStory = function(){
+      if($scope.inputForm.$valid)$scope.view = 'story';
+    };
+
+    $scope.view = 'input';
   });
