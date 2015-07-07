2dbd42632bf4dde4cc30ea9281aae32e8752e653
Austin Adams <austin@farmingtonwebdesign.net>
2015-07-07 10:54:59 -0700

    added reset button

--- script.js
+++ script.js
@@ -10,26 +10,32 @@ angular.module('myApp', [])
     };
   })
   .controller("storyController", function ($scope) {
-    $scope.name,
-      $scope.job_title,
-      $scope.tedious_task,
-      $scope.celebrity,
-      $scope.dirty_task,
-      $scope.useless_skill,
-      $scope.adjective,
-      $scope.obnoxiuous_celebrity,
-      $scope.huge_number = "";
-    $scope.gender = 'F';
+    var init = function() {
+      $scope.name,
+        $scope.job_title,
+        $scope.tedious_task,
+        $scope.celebrity,
+        $scope.dirty_task,
+        $scope.useless_skill,
+        $scope.adjective,
+        $scope.obnoxiuous_celebrity,
+        $scope.huge_number = "";
+      $scope.gender = 'F';
+      $scope.genderStrFn();
+      $scope.view = 'input';
+    };
     $scope.genderStrFn = function(){
       $scope.genderStr1 = $scope.gender === 'F' ? "her":"his";
       $scope.genderStr2 = $scope.gender === 'F' ? "she":"he";
       $scope.genderStr3 = $scope.gender === 'F' ? "her":"him";
     };
-    $scope.genderStrFn();
+    init();
 
     $scope.buildStory = function(){
       if($scope.inputForm.$valid)$scope.view = 'story';
     };
 
-    $scope.view = 'input';
+    $scope.resetStory = function(){
+      init();
+    };
   });
